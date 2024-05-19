fbd2923bf92eea2c629841be0071711499a6e823
atakowiec <mat.cieszczyk@gmail.com>
2024-05-19 18:48:16 +0200

    add validation

--- app/src/main/java/pl/pollub/android/app_2/PhoneActivity.java
+++ app/src/main/java/pl/pollub/android/app_2/PhoneActivity.java
@@ -7,48 +7,62 @@ import static pl.pollub.android.app_2.PhonesListActivity.PHONE_MODEL_KEY;
 import static pl.pollub.android.app_2.PhonesListActivity.PHONE_WEB_SITE_KEY;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.net.URL;
+
 public class PhoneActivity extends AppCompatActivity {
+    private PhoneViewModel phoneViewModel;
+    private Intent editIntent;
     private EditText manufacturerEt;
     private EditText modelEt;
     private EditText androidVersionEt;
     private EditText webSiteEt;
-    private Button webSiteBt;
-    private Button cancelBt;
-    private Button saveBt;
-    private PhoneViewModel phoneViewModel;
     private long phoneId;
-    private String phoneManufacturer;
-    private String phoneModel;
-    private String phoneAndroidVersion;
-    private String phoneWebSite;
-    private Intent editIntent;
-
+    private TextView manufacturerErrorTv;
+    private TextView modelErrorTv;
+    private TextView androidVersionErrorTv;
+    private TextView webSiteErrorTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone);
         this.phoneViewModel = new ViewModelProvider(this).get(PhoneViewModel.class);
+
         this.manufacturerEt = this.findViewById(R.id.manufacturer_et);
         this.modelEt = this.findViewById(R.id.model_et);
         this.androidVersionEt = this.findViewById(R.id.android_version_et);
         this.webSiteEt = this.findViewById(R.id.web_site_et);
-        this.webSiteBt = this.findViewById(R.id.web_site_bt);
-        this.cancelBt = this.findViewById(R.id.cancel_bt);
-        this.saveBt = this.findViewById(R.id.save_bt);
+
+        this.manufacturerErrorTv = this.findViewById(R.id.manufacturer_error_tv);
+        this.modelErrorTv = this.findViewById(R.id.model_error_tv);
+        this.androidVersionErrorTv = this.findViewById(R.id.android_version_error_tv);
+        this.webSiteErrorTv = this.findViewById(R.id.web_site_error_tv);
+
         loadPhone();
 
-        this.saveBt.setOnClickListener(view -> savePhone());
-        this.cancelBt.setOnClickListener(view -> {
+        this.findViewById(R.id.save_bt).setOnClickListener(view -> savePhone());
+        this.findViewById(R.id.cancel_bt).setOnClickListener(view -> {
             setResult(RESULT_CANCELED);
             finish();
+        });
+        this.findViewById(R.id.web_site_bt).setOnClickListener(view -> {
+            // app will take current value from the web site field and try to open it in the browser
+            String webSite = this.webSiteEt.getText().toString();
+            if (webSite.isEmpty()) return;
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(webSite));
+            startActivity(intent);
         });
     }
 
@@ -61,32 +75,90 @@ public class PhoneActivity extends AppCompatActivity {
 
         long id = bundle.getLong(PHONE_ID_KEY);
         this.phoneId = id;
-        if (id > 0) {
-            this.phoneManufacturer = bundle.getString(PHONE_MANUFACTURER_KEY);
-            this.phoneModel = bundle.getString(PHONE_MODEL_KEY);
-            this.phoneAndroidVersion = bundle.getString(PHONE_ANDROID_VERSION_KEY);
-            this.phoneWebSite = bundle.getString(PHONE_WEB_SITE_KEY);
-            this.manufacturerEt.setText(String.valueOf(this.phoneManufacturer));
-            this.modelEt.setText(this.phoneModel);
-            this.androidVersionEt.setText(this.phoneAndroidVersion);
-            this.webSiteEt.setText(this.phoneWebSite);
-        }
+        if (id <= 0) return;
+
+        String phoneManufacturer = bundle.getString(PHONE_MANUFACTURER_KEY);
+        String phoneModel = bundle.getString(PHONE_MODEL_KEY);
+        String phoneAndroidVersion = bundle.getString(PHONE_ANDROID_VERSION_KEY);
+        String phoneWebSite = bundle.getString(PHONE_WEB_SITE_KEY);
+        this.manufacturerEt.setText(String.valueOf(phoneManufacturer));
+        this.modelEt.setText(phoneModel);
+        this.androidVersionEt.setText(phoneAndroidVersion);
+        this.webSiteEt.setText(phoneWebSite);
     }
 
     private void savePhone() {
         Bundle bundle = new Bundle();
         Intent intent = new Intent();
 
-        // todo add validation
+        String manufacturer = this.manufacturerEt.getText().toString();
+        String model = this.modelEt.getText().toString();
+        String androidVersion = this.androidVersionEt.getText().toString();
+        String webSite = this.webSiteEt.getText().toString();
+
+        if (!validate(manufacturer, model, androidVersion, webSite)) return;
 
         bundle.putLong(PHONE_ID_KEY, this.phoneId);
-        bundle.putString(PHONE_MANUFACTURER_KEY, this.manufacturerEt.getText().toString());
-        bundle.putString(PHONE_MODEL_KEY, this.modelEt.getText().toString());
-        bundle.putString(PHONE_ANDROID_VERSION_KEY, this.androidVersionEt.getText().toString());
-        bundle.putString(PHONE_WEB_SITE_KEY, this.webSiteEt.getText().toString());
+        bundle.putString(PHONE_MANUFACTURER_KEY, manufacturer);
+        bundle.putString(PHONE_MODEL_KEY, model);
+        bundle.putString(PHONE_ANDROID_VERSION_KEY, androidVersion);
+        bundle.putString(PHONE_WEB_SITE_KEY, webSite);
 
         intent.putExtras(bundle);
         setResult(RESULT_OK, intent);
         finish();
+    }
+
+    private boolean validate(@Nullable String manufacturer, @Nullable String model, @Nullable String androidVersion, @Nullable String webSite) {
+        clearErrors();
+        boolean isValid = true;
+
+        if (manufacturer == null || manufacturer.isEmpty()) {
+            setError(this.manufacturerErrorTv, getString(R.string.error_empty_manufacturer));
+            isValid = false;
+        }
+
+        if (model == null || model.isEmpty()) {
+            setError(this.modelErrorTv, getString(R.string.error_empty_model));
+            isValid = false;
+        }
+
+        if (androidVersion == null || androidVersion.isEmpty()) {
+            setError(this.androidVersionErrorTv, getString(R.string.error_empty_android_version));
+            isValid = false;
+        }
+
+        if (webSite == null || webSite.isEmpty()) {
+            setError(this.webSiteErrorTv, getString(R.string.error_empty_web_site));
+            isValid = false;
+        } else {
+            // Check if the web site is a valid URL
+            try {
+                // this will throw an exception if the URL is not valid
+                new URL(webSite).toURI();
+            } catch (Exception e) {
+                setError(this.webSiteErrorTv, getString(R.string.error_invalid_web_site));
+                isValid = false;
+            }
+        }
+
+        return isValid;
+    }
+
+    private void setError(TextView errorTv, String message) {
+        errorTv.setText(message);
+        errorTv.setVisibility(View.VISIBLE);
+    }
+
+    private void clearErrors() {
+        this.manufacturerErrorTv.setText("");
+        this.modelErrorTv.setText("");
+        this.androidVersionErrorTv.setText("");
+        this.webSiteErrorTv.setText("");
+
+        this.manufacturerErrorTv.setVisibility(View.GONE);
+        this.modelErrorTv.setVisibility(View.GONE);
+        this.androidVersionErrorTv.setVisibility(View.GONE);
+        this.webSiteErrorTv.setVisibility(View.GONE);
     }
 }
